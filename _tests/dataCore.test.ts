594ddb613e898cc8d934e90baebc81aa18aed262
Unknown <danilodevs@gmail.com>
2018-08-30 17:10:11 -0400

    Rudimentary tests

--- _tests/dataCore.test.ts
+++ _tests/dataCore.test.ts
@@ -1,34 +1,18 @@
 import { DataCore } from "../src/core";
-import { IObjectDidChange } from "mobx";
 
-describe("Data Core", () => {
-    const baseData = {
+it("Works", () => {
+    const core = new DataCore({
         foo: "bar",
-        baz: {
-            hello: "world",
-            deep: {
-                again: "yes"
-            }
+        deep: {
+            sea: "water"
         }
-    };
-
-    it("Works", () => {
-        const core = new DataCore({
-            valueWillChange(change, path) {
-                change.newValue += " world";
-                return change;
-            }
-        }, baseData);
-
-        core.data.foo = "test";
-        core.data.baz.deep.again = "what";
-
-        expect(core.data.foo).toEqual("test world");
-        expect(core.data.baz.deep.again).toEqual("what world");
-
-        core.dispose();
-
-        core.data.foo = "blap";
-        expect(core.data.foo).toEqual("blap");
     });
+
+    core.$intercept.subscribe(event => {
+        event.change.newValue += "!";
+        event.acceptChange();
+    });
+
+    core.data.foo = "fubar";
+    expect(core.data.foo).toEqual("fubar!");
 });
--- _tests/server.test.ts
+++ _tests/server.test.ts
@@ -1,33 +1,45 @@
+import "reflect-metadata";
 import ServerIO from "socket.io";
 import ClientIO from "socket.io-client";
 import getPort from "get-port";
 import { ServerData } from "../src/server";
 import sleep from "then-sleep";
 import { ClientData } from "../src/client";
-import { when, reaction } from "mobx";
+import { when } from "mobx";
 
-it.only("Server Works", async () => {
+it("Server Works", async () => {
+    jest.setTimeout(300);
+
     const baseData = {
         foo: "bar",
         hello: {
-            world: "welcome!"
-        }
+            world: "welcome!",
+        },
     };
 
     const port         = await getPort(),
           socketServer = ServerIO(port),
           uri          = `http://localhost:${port}/`;
 
-    const serverData = new ServerData(socketServer, baseData),
-          data       = serverData.data;
+    const serverData = new ServerData(socketServer, baseData);
 
-    const clientData = await ClientData.connect<typeof baseData>(ClientIO(uri));
+    const socketClientA = ClientIO(uri);
+    const socketClientB = ClientIO(uri);
 
-    serverData.data.foo = "huh?";
-    clientData.data.foo = "foobar!";
+    const clientDataA = await ClientData.connect<typeof baseData>(socketClientA);
+    const clientDataB = await ClientData.connect<typeof baseData>(socketClientB);
 
-    // await sleep(30);
+    clientDataA.data.foo = "test";
 
-    // console.log(clientData.data.foo);
-    // console.log(serverData.data.foo);
+    await Promise.all([
+        when(() => serverData.data.foo === "test"),
+        when(() => clientDataB.data.foo === "test"),
+    ]);
+
+    serverData.data.foo = "foobar";
+    await when(() => serverData.data.foo === "foobar");
+
+    socketServer.close();
+    socketClientA.close();
+    socketClientB.close();
 });
